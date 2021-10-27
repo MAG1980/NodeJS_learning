ed34e813e13687972211e8d05ac8ebc52124c7de
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-10-27 14:13:50 +0300

    Удалось получить инкремент даты. Приступаю к прослушиванию события.

--- lesson2/index.js
+++ lesson2/index.js
@@ -2,16 +2,34 @@ const EventEmitter = require ('events')
 class MyEmitter extends EventEmitter {};
 const timeEmitter = new MyEmitter();
 
-// timeEmitter.on('hour', Handler.hour);
-// timeEmitter.on('day', Handler.day);
-// timeEmitter.on('month', Handler.month);
-// timeEmitter.on('year', Handler.year);
-
-console.log('Hi!')
 let input = process.argv[2]
-console.log(input)
+// console.log(input)
 let arr = input.split('-')
-console.log(arr)
+// console.log(arr)
+
+class Handler {
+	static hour (payload){
+		hourMessage = "Hour's are over";
+	}
+	static day (payload){
+		dayMessage = "Day's are over";
+	}
+	static month (payload){
+		monthMessage = "Month's are over"
+	}
+	static year (payload){
+		yearMessage = "Year's are over"
+	}
+}
+
+timeEmitter.once('hoursOver', Handler.hour);
+timeEmitter.once('daysOver', Handler.day);
+timeEmitter.once('monthsOver', Handler.month);
+timeEmitter.once('yearsOver', Handler.year);
+
+
+
+
 delay = (	timeUnit, unitValue) => {
 	let coefficient = 1
 
@@ -35,32 +53,66 @@ delay = (	timeUnit, unitValue) => {
 		}
 		return coefficient *unitValue;
 };
-let startDay = arr[1];
+
+let hourCounter = arr[0];
+let hourMessage = `часов: ${hourCounter}`;
+let hourIncrement = setInterval(() => {
+	if (dayCounter === 0) {
+		timeEmitter.emit('hoursOver');
+		clearInterval(hourIncrement);
+		return
+	}
+	hourMessage = `часов: ${hourCounter}`;
+	hourCounter = hourCounter - 1;
+}, 1000);
+
+let dayCounter = arr[1];
+let dayMessage = `дней: ${dayCounter}`;
 let dayIncrement = setInterval(() => {
-		if (startDay === 0) {
-			timeEmitter.emit('day');
+		if (dayCounter === 0) {
+			timeEmitter.emit('daysOver');
 			clearInterval(dayIncrement);
 			return
 		}
-	startDay = startDay - 1;
-}, 3000);
+	dayMessage = `дней: ${dayCounter}`;
+	dayCounter = dayCounter - 1;
+}, 2000);
+
+let monthCounter = arr[2];
+let monthMessage = `месяцев: ${monthCounter}`;
+let monthIncrement = setInterval(() => {
+	if (monthCounter === 0) {
+		timeEmitter.emit('monthsOver');
+		clearInterval(monthIncrement);
+		return
+	}
+	monthMessage = `месяцев: ${monthCounter}`;
+	monthCounter = monthCounter - 1;
+}, 4000);
+
+let yearCounter = arr[3];
+let yearMessage = `месяцев: ${yearCounter}`;
+let yearIncrement = setInterval(() => {
+	if (yearCounter === 0) {
+		timeEmitter.emit('yearsOver');
+		clearInterval(yearIncrement);
+		return
+	}
+	yearMessage = `лет: ${yearCounter}`;
+	yearCounter = yearCounter - 1;
+}, 8000);
+
+console.log('Hi!')
+console.log(`Для наглядности становлены длительности:
+1 час = 1 секунда
+1 день = 2 секунды
+1 месяц = 4 секуныды
+1 год = 8 секунд`)
+
 setInterval(() => {
-		console.log(`Осталось ${arr[0]} часов
-		${startDay} дней
-		${arr[2]} месяц(ев)
-		${arr[3]} лет.` )
-}, 3000)
-// class Handler {
-// 	static hour (payload){
-// 		console.log("Hour's is out")
-// 	}
-// 	static day (payload){
-// 		console.log("Day's is out")
-// 	}
-// 	static month (payload){
-// 		console.log("Month's is out")
-// 	}
-// 	static year (payload){
-// 		console.log("Year's is out")
-// 	}
-// }
+		console.log(`Осталось
+		${hourMessage}
+		${dayMessage}		
+		${monthMessage}	
+		${yearMessage}`)
+}, 1000)
