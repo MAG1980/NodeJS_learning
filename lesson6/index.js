9481626aadc67c6b75876f0b5ed539ee3d053fe5
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-11-10 10:49:37 +0300

    Добавил обработку события disconnect на стороне сервера.

---
+++ lesson6/index.js
@@ -0,0 +1,33 @@
+const socket = require("socket.io");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const indexPath = path.join(__dirname, "index.html");
+  const readStream = fs.createReadStream(indexPath);
+
+  readStream.pipe(res);
+});
+
+const io = socket(server);
+
+io.on("connection", (client) => {
+  console.log("New client connected!");
+
+  client.on("client-msg", (data) => {
+    // console.log(data);
+    const payload = {
+      message: data.message.split("").reverse().join(""),
+    };
+
+    client.broadcast.emit("server-msg", payload);
+    client.emit("server-msg", payload);
+  });
+
+  client.on("disconnect", () => {
+    console.log("Client is disconnected");
+  });
+});
+
+server.listen(5555);
