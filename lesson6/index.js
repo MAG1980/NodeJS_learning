00772e9c379a4c7de854afe0ba6e399eda11d6e4
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2021-11-11 18:00:14 +0300

    Переподключение пользователей и учёт их количества на сервере уже работает. Начинаю дорабатывать фронтенд.

--- lesson6/index.js
+++ lesson6/index.js
@@ -16,34 +16,51 @@ const server = http.createServer((req, res) => {
 
 const io = socket(server);
 
+var allClients = [];
+
+io.sockets.on("connection", function (socket) {
+  allClients.push(socket);
+  // console.log(allClients);
+  console.log(allClients);
+  console.log(allClients.length);
+});
+
 io.on("connection", (client) => {
-  console.log("New client connected!");
+  console.log(`New client with id: ${client.id} connected!`);
+  // console.log(client);
+  // console.log(client.id);
+
+  client.on("client-reconnection", (data) => {
+    client.id = data.userID;
+    client.userName = data.userName;
+    console.log(
+      `Client ${client.userName} with id: ${client.id} id reconnected!`
+    );
+    console.log(allClients.length);
+  });
 
   client.on("client-username", (data) => {
     console.log(data);
-    if (data.userID) {
-      userID = nanoid();
-    } else {
-      userID = data.userID;
-    }
+    client.userName = data.userName;
+
     const payload = {
-      userName: data.userName,
-      message: `I'm connected`,
+      userName: client.userName,
+      message: `${client.userName} is connected`,
     };
 
     const payloadWithUserID = {
       ...payload,
-      userID,
+      userID: client.id,
     };
     console.log(payloadWithUserID);
     client.broadcast.emit("server-msg", payload);
     client.emit("server-msg", payloadWithUserID);
-    console.log(payloadWithUserID.userID);
+    // console.log(payloadWithUserID.userID);
     DB_Users.push({
       userID: payloadWithUserID.userID,
       userName: payloadWithUserID.userName,
     });
-    console.log(DB_Users);
+    // console.log(DB_Users);
   });
 
   client.on("client-msg", (data) => {
@@ -58,10 +75,20 @@ io.on("connection", (client) => {
   });
 
   client.on("disconnect", () => {
-    console.log("Client is disconnected");
+    console.log(
+      `Client id: ${client.id} name: ${client.userName} is disconnected`
+    );
     let payload = {
       message: "Client is disconnected",
     };
+    console.log(allClients);
+    let disconnectedUser = allClients.filter((inArrClient) => {
+      return inArrClient.id === client.id;
+    });
+    console.log(disconnectedUser);
+    let disconnectedUserIndex = allClients.indexOf(disconnectedUser);
+    allClients.splice(disconnectedUserIndex, 1);
+    console.log(allClients.length);
     client.broadcast.emit("server-msg", payload);
     client.emit("server-msg", payload);
   });
